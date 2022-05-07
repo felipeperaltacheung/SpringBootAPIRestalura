69cb3bf03809c131e2ca6559191d717c62b07a2d
felipeperaltacheung <f33___@hotmail.com>
2022-05-06 21:08:31 -0300

    criacao do metodo remover para remover um topico

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -41,6 +41,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -61,6 +62,15 @@ public class TopicosController {
 		Topico topico = form.atualizar(id, topicoRepository);
 
 		return ResponseEntity.ok(new TopicoDto(topico));
+
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+
+		return ResponseEntity.ok().build();
 
 	}
 
